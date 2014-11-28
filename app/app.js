6d8ef0e3bb2c6f64cb7df5232bb607c9a264677c
vandero <robin.vandenbroeck@gmail.com>
2014-11-28 20:43:57 +0100

    Google drive integration

--- app/app.js
+++ app/app.js
@@ -2,11 +2,65 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }])
+
+    .controller('AppCtrl', function ($scope) {
+        var CLIENT_ID = '929279558954-p46q28jrcmrgn9fhshlc0c2vh2eeh6uv.apps.googleusercontent.com';
+        var SCOPES = 'https://www.googleapis.com/auth/drive';
+
+
+        /**
+         * Called when the client library is loaded to start the auth flow.
+         */
+        function handleClientLoad() {
+            window.setTimeout(checkAuth, 1);
+        }
+
+        /**
+         * Check if the current user has authorized the application.
+         */
+        function checkAuth() {
+            gapi.auth.authorize(
+                {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
+                handleAuthResult);
+        }
+
+        /**
+         * Called when authorization server replies.
+         *
+         * @param {Object} authResult Authorization result.
+         */
+        function handleAuthResult(authResult) {
+            /*
+            var authButton = document.getElementById('authorizeButton');
+            var filePicker = document.getElementById('filePicker');
+            authButton.style.display = 'none';
+            filePicker.style.display = 'none';
+            */
+            if (authResult && !authResult.error) {
+                // Access token has been successfully retrieved, requests can be sent to the API.
+                //filePicker.style.display = 'block';
+                //filePicker.onchange = uploadFile;
+                console.log("Authentication token successfully retrieved")
+            } else {
+                console.log("Retrying authentication")
+                // No access token could be retrieved, show the button to start the authorization flow.
+                //authButton.style.display = 'block';
+                //authButton.onclick = function() {
+                    gapi.auth.authorize(
+                        {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
+                        handleAuthResult);
+                //};
+            }
+        }
+
+        $scope.loggedOnUser = "Robin";
+    });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,90 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'angularFileUpload'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', function ($scope, FileUploader) {
 
-}]);
+        var uploader = $scope.uploader = new FileUploader();
+
+        uploader.onAfterAddingFile = function(item) {
+            console.log("Item added " + item);
+            insertFile(item.file);
+            item.cancel();
+        };
+
+        /**
+         * Insert new file.
+         *
+         * @param {File} fileData File object to read data from.
+         * @param {Function} callback Function to call when the request is complete.
+         */
+        function insertFile(fileData, callback) {
+            var boundary = '-------314159265358979323846';
+            var delimiter = "\r\n--" + boundary + "\r\n";
+            var close_delim = "\r\n--" + boundary + "--";
+
+            var reader = new FileReader();
+            reader.readAsBinaryString(fileData);
+            reader.onload = function(e) {
+                var contentType = fileData.type || 'application/octet-stream';
+                var metadata = {
+                    'title': fileData.name,
+                    'mimeType': contentType
+                };
+
+                var base64Data = btoa(reader.result);
+                var multipartRequestBody =
+                    delimiter +
+                    'Content-Type: application/json\r\n\r\n' +
+                    JSON.stringify(metadata) +
+                    delimiter +
+                    'Content-Type: ' + contentType + '\r\n' +
+                    'Content-Transfer-Encoding: base64\r\n' +
+                    '\r\n' +
+                    base64Data +
+                    close_delim;
+
+                var request = gapi.client.request({
+                    'path': '/upload/drive/v2/files',
+                    'method': 'POST',
+                    'params': {'uploadType': 'multipart'},
+                    'headers': {
+                        'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+                    },
+                    'body': multipartRequestBody});
+                if (!callback) {
+                    callback = function(file) {
+                        console.log(file)
+                    };
+                }
+                request.execute(callback);
+            }
+        }
+
+        $scope.hide = function() {
+            return false;
+        };
+
+        /**
+         * Start the file upload.
+         *
+         * @param {Object} evt Arguments from the file selector.
+         */
+        $scope.uploadFile = function($files) {
+            console.log("Starting upload");
+            alert("hello");
+            /*
+             gapi.client.load('drive', 'v2', function() {
+             //var file = evt.target.files[0];
+             insertFile($files[0]);
+             });
+             */
+        };
+    });
